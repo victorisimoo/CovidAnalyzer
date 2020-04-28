40256a937ac3bae12deee2d8d526f74042c7894c
Ale180820 <aylinneart@gmail.com>
2020-04-28 11:35:35 -0600

    Add Bootstrap and Priority Queue

--- CovidAnalyzer/Controllers/HomeController.cs
+++ CovidAnalyzer/Controllers/HomeController.cs
@@ -8,23 +8,10 @@ namespace CovidAnalyzer.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult InitialPage()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
